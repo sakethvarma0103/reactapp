4bd25d8a4d6ffc91b21caa58dc89c203b4593529
sakethvarma0103 <vdantulu4@gitam.in>
2024-02-20 00:24:40 +0530

    app.js

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+function App()
+{
+    return  (
+        <div><Header /><div className="note">
+            <h1>Note</h1>
+            <p>note</p>
+        </div><Footer /></div>
+    );
+}
+
+export  default App;
